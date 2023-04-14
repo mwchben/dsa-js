f2b9d825eb1f4bed9dec0f59afa6034b82df3bf1
Benjamin Mwangi <benjamwa05@gmail.com>
2023-04-14 17:02:56 +0300

    iteration

--- arrays.js
+++
@@ -1,4 +0,0 @@
-var a = [23,4,556,23,5646,88,35]
-console.log(a)
-console.log(a.length)
-console.log(a[0])
---
+++ dsa-sample.js
@@ -0,0 +1,15 @@
+// arrays 
+
+var a = [23,4,556,23,5646,88,35]
+console.log(a)
+console.log(a.length)
+console.log(a[0])
+
+//iterations
+
+for (let i = 0, sum = 0; i < a.length; i++) {
+    sum+=a[i]
+    console.log(sum);
+}
+
+
