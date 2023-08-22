a64a9bd86a13a5823e491e364aab885501a144bc
Benjamin Chege <benjamwa05@gmail.com>
2023-08-22 14:57:48 +0300

    trial

--- arrays.js
+++ arrays.js
@@ -11,3 +11,17 @@ console.log("subarray is: " + subarray);
 const lastElementInArray = array[2][2][1]
 console.log("last element in array is: " + lastElementInArray)
 
+const addElement = array.push(7) //adds at end, to add in array beginning use unshift()
+console.log("last updated element in array is: " + addElement)
+
+console.log("Array is now: " + array)
+
+const removedElement = array.pop()  //removes the last, to remove the 1st use shift()
+console.log("Array is now: " + array + " after the pop")
+console.log("Popped element is stored in the removed Element as: " + removedElement )
+
+
+
+
+
+
