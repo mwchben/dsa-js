dadfca0b91c793be8f0d1ef1991b9f28d367617b
Benjamin Chege <benjamwa05gmail.com>
2023-08-22 13:21:25 +0300

    arrays

---
+++ arrays.js
@@ -0,0 +1,13 @@
+const array = [
+    1,
+    2,
+    [3,4,[5,6]]
+]
+console.log("Array is: " + array)
+
+const subarray = array[2]
+console.log("subarray is: " + subarray);
+
+const lastElementInArray = array[2][2][1]
+console.log("last element in array is: " + lastElementInArray)
+
