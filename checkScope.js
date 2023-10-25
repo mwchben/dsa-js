ea9bf947aa4fde5726d7191829caf34976ce785a
Benjamin Chege <benjamwa05@gmail.com>
2023-10-25 08:18:38 +0300

    updated readme

--- checkScope.js
+++ checkScope.js
@@ -1,7 +1,7 @@
 function checkScope() {
     var i = 'function scope'; //here, let is limited in the if block scope/if var still same
     if (true) {
-       var i = 'block scope'; //here, let is limited in the if block scope/if var it assumes the global scope
+       let i = 'block scope'; //here, let is limited in the if block scope/if var it assumes the global scope
       console.log('Block scope i is: ', i);
     }
     console.log('Function scope i is: ', i);
@@ -9,3 +9,16 @@ function checkScope() {
   }
 
   checkScope()
+
+  /*here, we use rest parameter to define many arguments and get their sum of 
+  each of the elements in the arguments(numbers)*/
+  const sum = (...args) => {
+
+    let total = 0;
+    for (let i = 0; i < args.length; i++) {
+      total += args[i];
+    }
+    return total;
+  }
+
+  console.log(sum(7,5,4))
