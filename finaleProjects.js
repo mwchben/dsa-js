88c23ed5b14455f9bfcd942e2e258f26b922a0ad
Benjamin Chege <benjamwa05@gmail.com>
2023-12-30 19:25:55 +0300

    finale

--- finaleProjects.js
+++ finaleProjects.js
@@ -98,3 +98,65 @@ function telephoneCheck(str) {
 
   return validPatterns.some((pattern) => pattern.test(str));
 }
+
+//cashRegister
+function checkCashRegister(price, cash, cid) {
+  let change = cash*100 - price*100;
+  let drawer = 0;
+
+  for (let item of cid) {
+    drawer += item[1]*100;
+  }
+  if (change > drawer) {
+    return { status: "INSUFFICIENT_FUNDS", change: [] };
+  } else if (change === drawer) {
+    return { status: "CLOSED", change: cid };
+  } else {
+    let answer = [];
+    let units = {
+      "ONE HUNDRED": 10000,
+      "TWENTY": 2000,
+      "TEN": 1000,
+      "FIVE": 500,
+      "ONE": 100,
+      "QUARTER": 25,
+      "DIME": 10,
+      "NICKEL": 5,
+      "PENNY": 1,
+    };
+
+    for (let curr of cid.reverse()) {
+      let holder = [curr[0], 0];
+      curr[1] = curr[1]*100
+      while (change >= units[curr[0]] && curr[1] > 0) {
+        change -= units[curr[0]];
+        curr[1] -= units[curr[0]];
+        holder[1] += units[curr[0]]/100;
+      }
+
+      if (holder[1] > 0) {
+        answer.push(holder);
+      }
+    }
+    if (change > 0) {
+      return { status: "INSUFFICIENT_FUNDS", change: [] };
+    }
+
+    return { status: "OPEN", change: answer };
+  }
+}
+
+console.log(
+  checkCashRegister(3.26, 100, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
+  ])
+);
+
