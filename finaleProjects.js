082d0b918722f3e146b1fa0330a555bbb1c9376c
Benjamin Chege <benjamwa05@gmail.com>
2023-12-30 13:37:55 +0300

    telephone

--- finaleProjects.js
+++ finaleProjects.js
@@ -69,3 +69,32 @@ function rot13(str) {
 }
 
 rot13("SERR PBQR PNZC");
+
+
+//regex for telephones
+function telephoneCheck(str) {
+    const validPatterns = [
+        // 555-555-5555
+        /^\d{3}-\d{3}-\d{4}$/,
+
+        // 1 555-555-5555
+        /^1 \d{3}-\d{3}-\d{4}$/,
+
+        // 1 (555) 555-5555
+        /^1 \(\d{3}\) \d{3}-\d{4}$/,
+
+        // 5555555555
+        /^\d{10}$/,
+
+        // (555)555-5555
+        /^\(\d{3}\)\d{3}-\d{4}$/,
+
+        // 1 555 555 5555
+        /^1 \d{3} \d{3} \d{4}$/,
+
+        // 1(555)555-5555
+        /1\(\d{3}\)\d{3}-\d{4}/
+    ];
+
+  return validPatterns.some((pattern) => pattern.test(str));
+}
